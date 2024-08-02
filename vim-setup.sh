c9a5b78136e0b1afb8e286e87499005bb80ccb15
tsc11539 <tsc410195@gmail.com>
2024-08-02 18:16:36 +0800

    cp .vimrc to home dir/ add PlugInstall in sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -11,3 +11,9 @@ sudo apt install exuberant-ctags
 # install vim-plug
 sudo apt install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# move .vimrc to the home directory
+cp .vimrc ~/
+
+# run PlugInstall in command line
+vim +'PlugInstall --sync' +qa
